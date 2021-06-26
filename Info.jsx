5ed2b53ef448de77910aaca9b4b5f3722b22df77
Narapureddy Lakshmi Prasanna <lakshmi.76740@gmail.com>
2021-06-26 21:58:14 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>JavaScript is the world's most popular programming language.</p>
+    </div>
+  );
+}
+export default Info;
